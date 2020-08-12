f51b79b68aa1b4e09ca98f3bce0b22abaa91a5ab
Shubham Madhavi <60054243+Unknown-2020@users.noreply.github.com>
2020-08-12 14:26:21 +0530

    Add files via upload

---
+++ problem1_1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  8 12:50:46 2020
+
+@author: acer
+"""
+
+#PROBLEM STATEMENT 1
+def problem1_1():
+    print("Problem Set 1")
+    
+#a = problem1_1()
